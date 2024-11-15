8804ed9b78144b04d44bf2cfa09d976041a478c7
柳鹏 <1105094558@qq.com>
2024-11-15 10:11:35 +0800

    忽略电池优化功能

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,9 +1,10 @@
 package com.example.myapplication;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        View button1 = findViewById(R.id.button1);
+        View button2 = findViewById(R.id.button2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity2.this,MyService.class);
+                startService(intent);
+            }
         });
-
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-
-        // 使用获取到的数据，例如显示在TextView上
-        TextView textView = findViewById(R.id.textView3);  // 确保TextView的ID正确
-        textView.setText("Name: " + name);
-
-
-
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity2.this,MyService.class);
+                stopService(intent);
+            }
+        });
     }
-    public void MainActivity2a(View view) {
-        startActivity(new Intent(this,MainActivity2a.class));
-    }
+
+
 
 }
